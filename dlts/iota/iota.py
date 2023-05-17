407dc8953fbbf9320be6036a3565bdb87cce1771
root <root@ubuntu2004.linuxvmimages.local>
2023-05-16 21:55:07 -0400

    :hammer: code refactor

--- dlts/iota/iota.py
+++ dlts/iota/iota.py
@@ -2,89 +2,148 @@ from fogbed import (
     Container, VirtualInstance, FogbedExperiment
 )
 from typing import List
-import subprocess
-
+from typing import Dict
+import os
+import json
+import time
 
 class IotaBasic:
     def __init__(
         self,
         exp: FogbedExperiment,
-        prefix: str = 'node'
+        prefix: str = 'cloud'
     ) -> None:
         self.ledgers: List[VirtualInstance] = []
-        self.nodes: List[Container] = []
+        self.nodes: Dict[str, Container] = {}
         self.exp = exp
         self.prefix = prefix
 
-    # def create_ledgers(self, number_nodes: int):
-    #     self.ledgers = self._create_ledgers(number_nodes, self.prefix)
-    #     self.nodes = self._create_nodes(self.prefix)
-    #     return self.ledgers, self.nodes
-    
-    def create_ledger(self, prefix_node: str = 'node'):
-        ledger = self._create_ledger(1, self.prefix)
+    def add_ledger(self, prefix: str, nodes: List[Container]):
+        ledger = self.exp.add_virtual_instance(f'{prefix}')
+        self._create_nodes(ledger, nodes)
         self.ledgers.append(ledger)
-        node = self._create_node(prefix_node)
-        self.nodes.append(node)
-        return ledger, node
-
-    def create_links(self, target: VirtualInstance, devices: List[VirtualInstance]):
-        for device in devices:
-            self.exp.add_link(device, target)
-
-    # def _create_ledgers(self, number: int, prefix: str) -> List[VirtualInstance]:
-    #     return [self.exp.add_virtual_instance(f'{prefix}{i+1}') for i in range(number)]
-
-    def _create_ledger(self, prefix: str) -> List[VirtualInstance]:
-        return self.exp.add_virtual_instance(f'{prefix}')
-
-    def _create_node(self, prefix:str, ledger: VirtualInstance):
-        self.cli_instance = self.exp.add_virtual_instance(f'{prefix}_cli')
-        name = f'{prefix}-node'
-        node = Container(
-            name=name,
-            dimage='gohornet/hornet:1.2.1'
-        )
-        self.exp.add_docker(
-            container=node,
-            datacenter=ledger)
-        return node
+        return self.ledgers
     
-    def _create_nodes(self, prefix:str):
-        self.cli_instance = self.exp.add_virtual_instance(f'{prefix}')
-        nodes = []
-        for i, ledger in enumerate(self.ledgers):
-            name = f'node{i+1}'
-            node = Container(
-                name=name,
-                dimage='gohornet/hornet:1.2.1'
-            )
-            nodes.append(node)
+    def _create_nodes(self, ledger: VirtualInstance, nodes: List[Container]):
+        for node in nodes:
             self.exp.add_docker(
                 container=node,
                 datacenter=ledger)
+            self.nodes[f'{self.prefix}_{ledger.label}_{node.name}'] = node
         return nodes
     
-    def _create_node(self, prefix:str):
-        self.cli_instance = self.exp.add_virtual_instance(f'{prefix}_cli')
-        name = f'node{i+1}'
-        node = Container(
-            name=name,
-            dimage='gohornet/hornet:1.2.1'
-        )
-        self.nodes.append(node)
-        self.exp.add_docker(
-            container=node,
-            datacenter=ledger)
-        return node
+    @staticmethod
+    def write_file(file_path, file_name, text):
+        with open(file_path + '/' + file_name, 'w') as file:
+            file.write(text)
+
+    @staticmethod
+    def create_peer_conf_file(peer_conf_file, peerIp, peerName, peerID):
+        with open(peer_conf_file, "r") as f:
+            data = json.load(f)
+        data["peers"].append({"alias": peerName, "multiAddress": f"/dns/{peerIp}/tcp/15600/p2p/{peerID}"})
+        with open(peer_conf_file, "w") as f:
+            json.dump(data, f, indent=4)
+
+    @staticmethod
+    def setCooPublicKey(public_key, file_path):
+        with open(file_path, 'r') as f:
+            file_contents = f.read()
+            file_contents = file_contents.replace('"key": "{}"'.format(file_contents.split(
+                '"key": "')[1].split('"')[0]), '"key": "{}"'.format(public_key))
+        with open(file_path, 'w') as f:
+            f.write(file_contents)
+
+    def searchNode(self, node_name:str):
+        for node in self.nodes.values():
+            if node.name == node_name:
+                return node
+
+    # P2P identities are generated for each node
+    def setupIdentities(self):
+        print("\nGenerating P2P identities for each node")
+        for node in self.nodes.values():
+            nodeIdentity = node.cmd(f'./hornet tool p2pidentity-gen > {node.name}.identity.txt && cat {node.name}.identity.txt')
+            arq_name = f'{node.name}.identity.txt'
+            path = os.path.abspath('iota')
+            IotaBasic.write_file(path, arq_name, nodeIdentity)
+        print("P2P identities generated! ✅")
+    
+    # Extracts the peerID from the identity file
+    def extractPeerID(self):
+        print("\nExtracting peerID from the identity file")
+        for node_ext in self.nodes.values():
+            peerID = node_ext.cmd(
+            f'cat {node_ext.name}.identity.txt | awk -F : \'{{if ($1 ~ /PeerID/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
+            for node_int in self.nodes.values():
+                if node_int.name != node_ext.name:
+                    IotaBasic.create_peer_conf_file(os.path.abspath(f'iota/config/peering-{node_int.name}.json'),node_ext.ip, node_ext.name, peerID)        
+        print("peerID extracted! ✅")
+
+    # We need this so that the peering can be properly updated
+    def setPermissions(self):
+        if not os.uname()[0] == 'Darwin':
+            print("\nSetting permissions for the peering files")
+            for node in self.nodes.values():
+                path =  os.path.abspath(f"iota/config/peering-{node.name}.json")
+                if os.path.exists(path):
+                    os.system(f'sudo chown 65532:65532 {os.path.abspath(f"iota/config/peering-{node.name}.json")}')
+            print("Permissions set! ✅")
+    
+    #Sets the Coordinator up by creating a key pair
+    def setupCoordinator(self):
+        print("\nSetting up the Coordinator")
+        coo_key_pair_file="coo-milestones-key-pair.txt"
+        coo = self.searchNode("coo")
+        if coo is not None:
+            coo.cmd(f'./hornet tool ed25519-key > {coo_key_pair_file}')
+            COO_PRV_KEYS = coo.cmd(f'cat {coo_key_pair_file} | awk -F : \'{{if ($1 ~ /private key/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
+            coo.cmd(f'export COO_PRV_KEYS={COO_PRV_KEYS}')
+            coo_public_key= coo.cmd(f'cat {coo_key_pair_file} | awk -F : \'{{if ($1 ~ /public key/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
+            os.system(f'echo {coo_public_key} > {os.path.abspath("iota/coo-milestones-public-key.txt")}'.format(coo_public_key))
+            for node in self.nodes.values():
+               IotaBasic.setCooPublicKey(coo_public_key, os.path.abspath(f"iota/config/config-{node.name}.json"))
+            print("Coordinator set up! ✅")
+        else:
+            print("Coordinator not found! ❌")
+
+    # Bootstraps the coordinator
+    def bootstrapCoordinator(self):
+        print("Bootstrapping the Coordinator...")
+        # Need to do it again otherwise the coo will not bootstrap
+        if not os.uname()[0] == 'Darwin':
+            os.system(f'sudo chown 65532:65532 {os.path.abspath("iota/p2pstore")}')
+        coo = self.searchNode("coo")
+        if coo is not None:
+            coo.cmd(f'./hornet --cooBootstrap --cooStartIndex 0 > coo.bootstrap.log &')
+            print("Waiting for $bootstrap_tick seconds ... ⏳")
+            time.sleep(30)
+            bootstrapped = coo.cmd('grep "milestone issued (1)" coo.bootstrap.log | cat')
+            if(bootstrapped):
+                print("Coordinator bootstrapped successfully! ✅")
+                coo.cmd(f'pkill -f "hornet --cooBootstrap --cooStartIndex 0"')
+                coo.cmd('rm ./coo.bootstrap.container')
+                time.sleep(10)
+            else:
+                print("Error. Coordinator has not been boostrapped.")
+        else:
+            print("Coordinator not found! ❌")
+
+    def startContainers(self):
+        print("Starting the containers...")
+        for node in self.nodes.values():
+            node.cmd(f'./hornet > {node.name}.log &')
+            print(f"Starting {node.name}... ⏳")
+            time.sleep(10)
+            print(f"{node.name} is up and running! ✅")
 
     def start_network(self):
-        for node in self.nodes:
-            node.start()
-            node.cmd('tool ed25519-key > key-pair.txt')
-        # node.cmd('tool ed25519-key > key-pair.txt')
-
-        # ips = list(map(lambda node: node.ip, self.nodes))
-        # count_nodes = len(self.nodes)
-        # ips = ",".join(ips)
-        # print(ips)
+        print("Starting the network...")
+        self.setupIdentities()
+        self.extractPeerID()
+        self.setPermissions()
+        self.setupCoordinator()
+        self.bootstrapCoordinator()
+        self.startContainers()
+        print("Network is up and running! ✅")
+       
--- dlts/test-iota.py
+++ dlts/test-iota.py
@@ -6,52 +6,18 @@ from fogbed import (
     setLogLevel,
 )
 import os
-import json
-import time
 
 setLogLevel('info')
-
 
 def create_links(cloud: VirtualInstance, devices: List[VirtualInstance]):
     for device in devices:
         exp.add_link(device, cloud)
 
-
-def create_peer_conf_file(peer_conf_file, peerIp, peerName, peerID):
-    with open(peer_conf_file, "r") as f:
-        data = json.load(f)
-    data["peers"].append({"alias": peerName, "multiAddress": f"/dns/{peerIp}/tcp/15600/p2p/{peerID}"})
-    with open(peer_conf_file, "w") as f:
-        json.dump(data, f, indent=4)
-
-
-def setCooPublicKey(public_key, file_path):
-    with open(file_path, 'r') as f:
-        file_contents = f.read()
-        file_contents = file_contents.replace('"key": "{}"'.format(file_contents.split(
-            '"key": "')[1].split('"')[0]), '"key": "{}"'.format(public_key))
-    with open(file_path, 'w') as f:
-        f.write(file_contents)
-
-
-def setEntryNode(node, file_path):
-    with open(file_path, "r") as f:
-        data = json.load(f)
-    data["p2p"]["autopeering"]["entryNodes"] = [node]
-    with open(file_path, "w") as f:
-        json.dump(data, f, indent=4)
-
-def write_file(file_path, file_name, text):
-    with open(file_path + '/' + file_name, 'w') as file:
-        file.write(text)
-
 if (__name__ == '__main__'):
     exp = FogbedExperiment()
-    iota = IotaBasic(exp=exp)
-    cloud = exp.add_virtual_instance('cloud')
+    iota = IotaBasic(exp=exp, prefix='cloud')
 
-    ### NODE1 ###
-    ledger1 = exp.add_virtual_instance('ledger1')
+    ### NODES ###
     node1 = Container(
         name='node1',
         dimage='hornet',
@@ -65,12 +31,8 @@ if (__name__ == '__main__'):
                        '8081': '8081', '15600': '15600'},
         ports=['14265', '8081', '1883', '15600', '14626/udp']
     )
-    exp.add_docker(
-        container=node1,
-        datacenter=ledger1)
-    exp.add_link(ledger1, cloud)
+    iota.add_ledger('ledger1', [node1])
 
-    ledger5 = exp.add_virtual_instance('ledger5')
     node2 = Container(
         name='node2',
         dimage='hornet',
@@ -84,13 +46,9 @@ if (__name__ == '__main__'):
         ports=['14265', '8081', '15600', '14626/udp']
 
     )
-    exp.add_docker(
-        container=node2,
-        datacenter=ledger5)
-    exp.add_link(ledger5, cloud)
+    iota.add_ledger('ledger2', [node2])
 
     ### COO ###
-    ledger2 = exp.add_virtual_instance('ledger2')
     coo = Container(
         name='coo',
         dimage='hornet',
@@ -104,13 +62,9 @@ if (__name__ == '__main__'):
         environment={'COO_PRV_KEYS': ''},
         ports=['15600']
     )
-    exp.add_docker(
-        container=coo,
-        datacenter=ledger2)
-    exp.add_link(ledger2, cloud)
+    iota.add_ledger('ledger3', [coo])
 
     ### spammer ###
-    ledger3 = exp.add_virtual_instance('ledger3')
     spammer = Container(
         name='spammer',
         dimage='hornet',
@@ -122,136 +76,16 @@ if (__name__ == '__main__'):
                  f"{os.path.abspath('iota/snapshots')}:/app/snapshots"],
         ports=['15600', '14626/udp']
     )
+    iota.add_ledger('ledger4', [spammer])
 
-    exp.add_docker(
-        container=spammer,
-        datacenter=ledger3)
-    exp.add_link(ledger3, cloud)
-
-    ### node-autopeering ###
-    ledger4 = exp.add_virtual_instance('ledger4')
-    node_autopeering = Container(
-        name='auto',
-        dimage='hornet',
-        volumes=[f"{os.path.abspath('iota/config/config-autopeering.json')}:/app/config.json:ro",
-                 f"{os.path.abspath('iota/config/profiles.json')}:/app/profiles.json",
-                 f"{os.path.abspath('iota/db/private-tangle/node-autopeering.db')}:/app/db",
-                 f"{os.path.abspath('iota/p2pstore/node-autopeering')}:/app/p2pstore"],
-        port_bindings={'14626': '14626/udp'},
-        ports=['14626/udp']
-    )
-
-    exp.add_docker(
-        container=node_autopeering,
-        datacenter=ledger4)
-    exp.add_link(ledger4, cloud)
+    cloud = exp.add_virtual_instance('cloud')
+    create_links(cloud, iota.ledgers)
 
     try:
         exp.start()
-
-        # P2P identities are generated
-        node1Identity = node1.cmd(
-            f'./hornet tool p2pidentity-gen > node1.identity.txt && cat node1.identity.txt')
-        node2Identity = node2.cmd(
-            f'./hornet tool p2pidentity-gen > node2.identity.txt && cat node2.identity.txt')
-        cooIdentity = coo.cmd(
-            f'./hornet tool p2pidentity-gen > coo.identity.txt && cat coo.identity.txt')
-        spammerIdentity = spammer.cmd(
-            f'./hornet tool p2pidentity-gen > spammer.identity.txt && cat spammer.identity.txt')
-        nodeAutopeeringIdentity = node_autopeering.cmd(
-            f'./hornet tool p2pidentity-gen > node-autopeering.identity.txt && cat node-autopeering.identity.txt')
-
-        write_file(os.path.abspath('iota'), 'node1.identity.txt', node1Identity)
-        write_file(os.path.abspath('iota'), 'node2.identity.txt', node2Identity)
-        write_file(os.path.abspath('iota'), 'coo.identity.txt', cooIdentity)
-        write_file(os.path.abspath('iota'), 'spammer.identity.txt', spammerIdentity)
-        write_file(os.path.abspath('iota'), 'node-autopeering.identity.txt', nodeAutopeeringIdentity)
-
-        # Extracts the peerID from the identity file
-        node1Identity = node1.cmd(
-            f'cat node1.identity.txt | awk -F : \'{{if ($1 ~ /PeerID/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        node2Identity = node2.cmd(
-            f'cat node2.identity.txt | awk -F : \'{{if ($1 ~ /PeerID/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        cooIdentity = coo.cmd(
-            f'cat coo.identity.txt | awk -F : \'{{if ($1 ~ /PeerID/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        spammerIdentity = spammer.cmd(
-             f'cat spammer.identity.txt | awk -F : \'{{if ($1 ~ /PeerID/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-
-        # Sets the peering configuration
-        create_peer_conf_file(os.path.abspath('iota/config/peering-coo.json'),node1.ip,"node1", node1Identity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-coo.json'),spammer.ip,"spammer", spammerIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-coo.json'),node2.ip,"node2", node2Identity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node1.json'),coo.ip,"coo", cooIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node1.json'),spammer.ip, "spammer", spammerIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node1.json'),node2.ip,"node2", node2Identity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node2.json'),coo.ip,"coo", cooIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node2.json'),spammer.ip, "spammer", spammerIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-node2.json'),node1.ip,"node1", node1Identity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-spammer.json'),node1.ip,"node1", node1Identity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-spammer.json'),coo.ip, "coo", cooIdentity)
-        create_peer_conf_file(os.path.abspath('iota/config/peering-spammer.json'),node2.ip,"node2", node2Identity)
-
-        # We need this so that the peering can be properly updated
-        if not os.uname()[0] == 'Darwin':
-            os.system(f'sudo chown 65532:65532 {os.path.abspath("iota/config/peering-node1.json")}')
-            os.system(f'sudo chown 65532:65532 {os.path.abspath("iota/config/peering-node2.json")}')
-            os.system(f'sudo chown 65532:65532 {os.path.abspath("iota/config/peering-spammer.json")}')
-
-        #Sets the autopeering configuration
-        print("Setting autopeering configuration")
-        entry_peerID = node_autopeering.cmd(
-            f'cat node-autopeering.identity.txt | awk -F : \'{{if ($1 ~ /public key \(base58\)/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        multiaddr = f"/dns/{node_autopeering.ip}/udp/14626/autopeering/{entry_peerID}"
-        setEntryNode(multiaddr, os.path.abspath("iota/config/config-node1.json"))
-        setEntryNode(multiaddr, os.path.abspath("iota/config/config-node2.json"))
-        setEntryNode(multiaddr, os.path.abspath("iota/config/config-spammer.json"))
-
-        #start autopeering
-        node_autopeering.cmd(f'./hornet > node_autopeering.log &')
-
-        #Sets the Coordinator up by creating a key pair
-        coo_key_pair_file="coo-milestones-key-pair.txt"
-        coo.cmd(f'./hornet tool ed25519-key > {coo_key_pair_file}')
-        COO_PRV_KEYS = coo.cmd(f'cat {coo_key_pair_file} | awk -F : \'{{if ($1 ~ /private key/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        coo.cmd(f'export COO_PRV_KEYS={COO_PRV_KEYS}')
-        coo_public_key= coo.cmd(f'cat {coo_key_pair_file} | awk -F : \'{{if ($1 ~ /public key/) print $2}}\' | sed "s/ \+//g" | tr -d "\n" | tr -d "\r"').strip("> >")
-        os.system(f'echo {coo_public_key} > {os.path.abspath("iota/coo-milestones-public-key.txt")}'.format(coo_public_key))
-        setCooPublicKey(coo_public_key, os.path.abspath("iota/config/config-node1.json"))
-        setCooPublicKey(coo_public_key, os.path.abspath("iota/config/config-node2.json"))
-        setCooPublicKey(coo_public_key, os.path.abspath("iota/config/config-spammer.json"))
-        setCooPublicKey(coo_public_key, os.path.abspath("iota/config/config-coo.json"))
-
-        # Bootstraps the coordinator
-        print("Bootstrapping the Coordinator...")
-
-        # Need to do it again otherwise the coo will not bootstrap
-        if not os.uname()[0] == 'Darwin':
-            os.system(f'sudo chown 65532:65532 {os.path.abspath("iota/p2pstore")}')
-
-        coo.cmd(f'./hornet --cooBootstrap --cooStartIndex 0 > coo.bootstrap.log &')
-        print("Waiting for $bootstrap_tick seconds ... ⏳")
-        time.sleep(30)
-        bootstrapped = coo.cmd('grep "milestone issued (1)" coo.bootstrap.log | cat')
-        if(bootstrapped):
-            print("Coordinator bootstrapped successfully! ✅")
-            coo.cmd(f'pkill -f "hornet --cooBootstrap --cooStartIndex 0"')
-            coo.cmd('rm ./coo.bootstrap.container')
-            time.sleep(10)
-        else:
-            print("Error. Coordinator has not been boostrapped.")
-
-        coo.cmd(f'./hornet > coo.log &')
-        spammer.cmd(f'./hornet > spammer.log &')
-        node1.cmd(f'./hornet > node1.log &')
-        node2.cmd(f'./hornet > node2.log &')
-
+        iota.start_network()
+        print("Experiment started")
         input('Press any key...')
-        # print(node1.cmd(f'ping -c 4 {node2.ip}'))
-        # exp.start_cli()
-        # iota.start_network()
-        # print(nodes[0].cmd(f'ping -c 4 {nodes[1].ip}'))
-        # exp.start_cli()
-        # input('Press any key...')
 
     except Exception as ex:
         print(ex)
